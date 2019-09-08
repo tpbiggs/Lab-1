6c20b84a50e46c4be1be1afca39aa6b6064d4123
tpbig <tpbig@DESKTOP-3T6GCV8>
2019-09-08 16:13:45 -0500

    2nd Edit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +10,21 @@
  * @author tpbig
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Enter Price of Sale:");
+        
+        float priceSale = keyboard.nextFloat();
+        float taxState = priceSale * .04F;
+        float taxCounty = priceSale * .02F;
+        float taxTotal = taxState + taxCounty;
+        float realTotal = taxTotal + priceSale;
+        
+        System.out.println("State Tax: \t" + taxState);
+        System.out.println("County Tax: \t" + taxCounty);
+        System.out.println("Total Tax: \t" + taxTotal );
+        System.out.println("Total Sale: \t" + realTotal);
+        
+    }
 }
